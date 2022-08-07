cadf4c100a8170eb89b8855076a5553c7e54a776
kkkkan <kan4649kan@gmail.com>
2022-08-07 18:05:59 +0900

    feat : 一番簡単なCanvas使用例を追加

--- app/src/main/java/com/kkkkan/composeappfordroidkaigi2022/MainActivity.kt
+++ app/src/main/java/com/kkkkan/composeappfordroidkaigi2022/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.kkkkan.composeappfordroidkaigi2022
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            SimpleCanvasView()
+        }
     }
+
+
+    @Composable
+    fun SimpleCanvasView() {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawRect(Color.Black)
+            inset(150.0f) {
+                drawLine(
+                    color = Color.White,
+                    start = Offset.Zero,
+                    end = Offset(size.width, size.height),
+                    strokeWidth = 10.0f
+                )
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewSimpleCanvasView() {
+        SimpleCanvasView()
+    }
+
 }
 
