f426c6134e4394d17701046cb9b10ef8a251f957
kkkkan <kan4649kan@gmail.com>
2022-08-07 17:45:38 +0900

    first commit

---
+++ app/src/main/java/com/kkkkan/composeappfordroidkaigi2022/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kkkkan.composeappfordroidkaigi2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
